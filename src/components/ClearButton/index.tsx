074488865bbca4963759eaf6f48d1951f5f32059
Leozartino <leozartino@gmail.com>
2020-05-27 16:48:08 -0300

    feat: implement ClearButton component

--- src/components/ClearButton/index.tsx
+++ src/components/ClearButton/index.tsx
@@ -1,1 +1,12 @@
 import React from "react";
+import "./style.css";
+
+const ClearButton = (props: any) => {
+  return (
+    <div className="clear-btn" onClick={props.handleClear}>
+      {props.children}
+    </div>
+  );
+};
+
+export default ClearButton;
