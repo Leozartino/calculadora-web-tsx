1c9c39f43a1d6321382521d0a21b0b9b684d0649
Leozartino <leozartino@gmail.com>
2020-05-26 23:12:04 -0300

    feat: add Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,1 +1,12 @@
 import React from "react";
+import "./style.css";
+
+interface propDTO {
+  input: string;
+}
+
+const Input = (props: propDTO) => {
+  return <div className="input">{props.input}</div>;
+};
+
+export default Input;
