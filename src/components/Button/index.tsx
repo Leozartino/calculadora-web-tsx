44ac4725ebd2dc493f35740bf6e5186d98b24820
Leozartino <leozartino@gmail.com>
2020-05-27 16:48:19 -0300

    feat: add imports

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import "./style.css";
 import isOperator from "../../util/isOperator";
 
-const Button: React.FC = (props) => {
+interface Props {
+  handleClick: any;
+  children: string;
+}
+
+const Button = (props: Props) => {
   return (
     <div
       className={`button-wrapper ${
         isOperator(props.children) ? null : "operator"
       } `}
+      onClick={() => props.handleClick(props.children)}
     >
       {props.children}
     </div>
